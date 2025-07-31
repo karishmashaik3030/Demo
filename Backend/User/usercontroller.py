1a862d13141526a5f9dc242a16a39398a2647baf
karishmashaik3030 <99020023+karishmashaik3030@users.noreply.github.com>
2025-07-31 12:06:32 +0530

    Create usercontroller.py

---
+++ Backend/User/usercontroller.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request, jsonify
+from User.usermodel import get_user_info, create_user
+from app import app
+
+@app.route("/", methods=["GET"])
+def get_user():
+    result = get_user_info()
+    return jsonify(result)
+
+@app.route("/", methods=["POST"])
+def post_user():
+    data = request.json
+    result = create_user(data)
+    return jsonify(result)
