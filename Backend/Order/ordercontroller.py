c0a8c74cda94d2e4f0405d0c4b6cc5f40085cd4b
karishmashaik3030 <99020023+karishmashaik3030@users.noreply.github.com>
2025-08-05 17:19:19 +0530

    Update ordercontroller.py

--- Backend/Order/ordercontroller.py
+++ Backend/Order/ordercontroller.py
@@ -3,11 +3,11 @@ from Order.ordermodel import get_order_info, create_order
 from app import app
 
 
-@app.route("/", methods=["GET"])
+@app.route("/get-orders", methods=["GET"])
 def get_order():
     return jsonify(get_order_info())
 
-@app.route("/", methods=["POST"])
+@app.route("/create-order", methods=["POST"])
 def post_order():
     data = request.json
     return jsonify(create_order(data))
