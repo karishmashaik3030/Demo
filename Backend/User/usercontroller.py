e6736a0cdfb77b9b615d870de7de22b53c5e193a
karishmashaik3030 <99020023+karishmashaik3030@users.noreply.github.com>
2025-08-06 10:50:34 +0530

    Update usercontroller.py

--- Backend/User/usercontroller.py
+++ Backend/User/usercontroller.py
@@ -2,12 +2,12 @@ from flask import Blueprint, request, jsonify
 from User.usermodel import get_user_info, create_user
 from app import app
 
-@app.route("/", methods=["GET"])
+@app.route("/get-users", methods=["GET"])
 def get_user():
     result = get_user_info()
     return jsonify(result)
 
-@app.route("/", methods=["POST"])
+@app.route("/create-users", methods=["POST"])
 def post_user():
     data = request.json
     result = create_user(data)
