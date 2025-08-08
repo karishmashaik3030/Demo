b32f12b8fcb999b5a5a2418e59acdc8eea8cf1c0
karishmashaik3030 <99020023+karishmashaik3030@users.noreply.github.com>
2025-08-08 16:21:39 +0530

    Update ordercontroller.py

--- Backend/Order/ordercontroller.py
+++ Backend/Order/ordercontroller.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from Order.ordermodel import get_order_info, create_order
 from app import app
 
-
+#orders
 @app.route("/get-orders", methods=["GET"])
 def get_order():
     return jsonify(get_order_info())
