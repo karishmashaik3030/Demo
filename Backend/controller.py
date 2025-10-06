b696dca252ce1b1bee1dac522966e964cd166a28
karishmashaik3030 <99020023+karishmashaik3030@users.noreply.github.com>
2025-10-06 14:22:51 +0530

    Update controller.py

--- Backend/controller.py
+++ Backend/controller.py
@@ -1,6 +1,6 @@
 from app import app
 from model import demo
-#code controller here
+#code controller here it is
 @app.route('/demotest',methods=['GET'])
 def fetch_code_from_repo():
   demo_obj = demo()
